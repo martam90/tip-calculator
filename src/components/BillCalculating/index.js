9ac9a7039b6e20918d2a0bce8c9fa477a25b41b5
martam90 <banas.m.marta@gmail.com>
2021-11-07 12:42:56 +0100

    update component layout upon pull request

--- src/components/BillCalculating/index.js
+++ src/components/BillCalculating/index.js
@@ -5,33 +5,21 @@ const BillCalculating = () => {
   return (
     <section>
       <div className="main__container--mobile main__container-desktop--left">
-        <div>
-          <p className="main__container-text main__container-text--dark">Bill</p>
-          <form>
-            <label htmlFor="inputBill">
+        <form>
+          <label htmlFor="inputBill" className="main__container-text main__container-text--dark">Bill
               <input id="inputBill" name="input-bill" className="input input--bill" type="number"></input>
-            </label>
-          </form>
-        </div>
-        <div>
+          </label>
           <p className="main__container-text main__container-text--dark">Select Tip %</p>
           <div className="main__container--buttons">
             <TipButton />
-            <form>
-              <label htmlFor="inputCustom">
-                <input id="inputCustom" name="input-custom" className="input input--custom" type="number" placeholder="Custom"></input>
-              </label>
-            </form>
+            <label htmlFor="inputCustom">
+              <input id="inputCustom" name="input-custom" className="input input--custom" type="number" placeholder="Custom"></input>
+            </label>
           </div>
-        </div>
-        <div>
-          <p className="main__container-text main__container-text--dark">Number of People</p>
-          <form>
-            <label htmlFor="inputPeople">
+            <label htmlFor="inputPeople" className="main__container-text main__container-text--dark">Number of People
               <input id="inputPeople" name="input-people" type="number" className="input input--people"></input>
             </label>
-          </form>
-        </div>
+        </form>
       </div>
     </section>
   );
