b6811d8a9e568a208e4f8a3b316b02430c7d351a
martam90 <banas.m.marta@gmail.com>
2021-11-08 18:39:21 +0100

    reorgenised button components

--- src/components/DisplayToPay/index.js
+++ src/components/DisplayToPay/index.js
@@ -1,27 +1,23 @@
 import React from 'react';
-import ResetButton from '../ResetButton';
 import billAmount from '../../assets/text.js';
 
 const DisplayToPay = () => {
   return (
-    <section>
-      <div className="main__container--mobile main__container-desktop--right">
-        <div className="main__container--mobile-bottom">
-          {billAmount.map((text) => (
-            <div className="main__container--amount-wrapper" key={text.id}>
-              <div>
-                <p className="main__container-text main__container-text--light">{text.paragraph}
-                <br></br>
-                <span className="main__container-text--dark-small">{text.span}</span></p>
-              </div>
-              <div className="main__container-text-bill">$0.00</div>
+    <div className="main__container--mobile main__container-desktop--right">
+      <div className="main__container--mobile-bottom">
+        {billAmount.map((text) => (
+          <div className="main__container--amount-wrapper" key={text.id}>
+            <div>
+              <p className="main__container-text main__container-text--light">{text.paragraph}
+              <br></br>
+              <span className="main__container-text--dark-small">{text.span}</span></p>
             </div>
-          ))}
-          <ResetButton />
-        </div>
+            <div className="main__container-text-bill">$0.00</div>
+          </div>
+        ))}
+        <button className="button button--reset" type="reset">Reset</button>
       </div>
-    </section>
-    
+    </div>
   );
 };
 
